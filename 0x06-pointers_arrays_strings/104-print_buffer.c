ad19e7aa6b8a8e4b79b133b7e6a1e21ae0846235
13Napoleon <napimulugeta@gmail.com>
2023-01-02 10:46:51 -0800

    a file that contains the password for the crackme2 executable.

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,10 +1,11 @@
+
 #include "main.h"
 #include <stdio.h>
 
 /**
  * print_buffer - Prints a buffer 10 bytes at a time, starting with
  *                the byte position, then showing the hex content,
- *                then displaying printable charcaters.
+ *OOB                then displaying printable charcaters.
  * @b: The buffer to be printed.
  BOB* @size: The number of bytes to be printed from the buffer.
  */
